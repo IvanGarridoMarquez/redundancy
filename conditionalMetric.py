00af3a3949f980e87a59ae1e08a8629a47ae3140
IvanGarridoMarquez <ivangarridom@yahoo.com>
2017-10-17 17:30:36 +0200

    type data error in operations

--- conditionalMetric.py
+++ conditionalMetric.py
@@ -44,7 +44,7 @@ for i in range(0,len(categs)):
     for j in range(i+1,len(categs)):
         #Here get the size of the intersection ietj between i and j 
         ietj=curBlog.getIntersectionSizeBetweenCatACatB(categs[i][0],categs[j][0])
-        pcondVal=pCond(int(ietj),int(categs[i][1]),int(categs[j][1]),N)
+        pcondVal=pCond(int(ietj[0]),int(categs[i][1]),int(categs[j][1]),N)
         matMetric[x][y]=pcondVal[0]
         matMetric[y][x]=pcondVal[1]
         X=categs[i][2]
